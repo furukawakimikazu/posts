97a457271535bf4aed032c186f1d992b43b1e2ee
furukawakimikazu <g.guquan@gmail.com>
2022-01-25 15:38:49 +0900

    編集画面の作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '73f23eeaa5013639bf9d870bdbc1eba97c5510dc402aa6540c81fd939f4550f479fe92269b38e66b6bfa88d8f86e6c62c0353dfb03457acbf8f87e40b4e17012'
