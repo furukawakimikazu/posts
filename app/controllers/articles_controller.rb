9f95619d369e7ae24140ad045503debdfd144f93
furukawakimikazu <g.guquan@gmail.com>
2022-01-27 11:46:45 +0900

    articleControllerのcreateActionの実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,19 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    @article.user_id = current_user.id
+    @article.save
+    redirect_to @article
+  end
+  
   def edit
   end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :image)
+  end
+  
 end
